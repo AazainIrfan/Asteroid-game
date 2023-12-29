408291b9f950a23e83a707bfc7ab6687d37c727b
Muhammad Aazain Irfan <116751216+AazainIrfan@users.noreply.github.com>
2023-12-28 22:50:07 -0500

    Drawable.java

---
+++ Drawable.java
@@ -0,0 +1,14 @@
+/**
+ * The Drawable interface defines a standard method for game elements 
+ * in the Asteroid Adventure Game that need to be drawn on the screen.
+ *
+ * @version 1.0
+ * @since 2023-04-04
+ */
+public interface Drawable {
+    
+    /**
+     * Draws the current state of the game element on the screen.
+     */
+    void draw();
+}
